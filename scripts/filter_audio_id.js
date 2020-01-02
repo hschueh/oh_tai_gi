01c2c03b6571eae4cb7e6613105ffd76ddd2fc78
hschueh <chueh044@gmail.com>
2020-01-02 10:40:37 +0800

    Fix the script. Should use async/await properly.

--- scripts/filter_audio_id.js
+++ scripts/filter_audio_id.js
@@ -19,6 +19,9 @@ const http = require('http');
 function getPromise(url) {
 	return new Promise((resolve, reject) => {
 		http.get(url, (response) => {
+            if(response.statusCode !== 200){
+				reject(response.statusCode);
+            }
 			let chunks_of_data = [];
 
 			response.on('data', (fragments) => {
@@ -37,18 +40,34 @@ function getPromise(url) {
 	});
 }
 
+function filterAsync(array, predicate) {
+    // Take a copy of the array, it might mutate by the time we've finished
+   const data = Array.from(array);
+   // Transform all the elements into an array of promises using the predicate
+   // as the promise
+   return Promise.all(data.map((element, index) => predicate(element, index, data)))
+   // Use the result of the promises to call the underlying sync filter function
+     .then(result => {
+       return data.filter((element, index) => {
+         return result[index];
+       });
+     });
+ }
+(async()=>{
 const output = [];
 var args = process.argv.slice(2);
-args.forEach(function (file) {
+await Promise.all(args.map(async function (file) {
   let inputs = require(`../assets/dict/${file}`);
-  inputs.forEach(function (vocabulary) {
-    vocabulary.heteronyms = vocabulary.heteronyms.filter(async function (heteronym) {
+  for(let i = 0; i < inputs.length; ++i) {
+    let vocabulary = inputs[i];
+    vocabulary.heteronyms = await filterAsync(vocabulary.heteronyms, async function (heteronym) {
         let aid = heteronym.audio_id?heteronym.audio_id:heteronym.id;
         aid = aid.padStart(5, "0");
         let url = `http://t.moedict.tw/${aid}.ogg`;
+        console.log(url);
         try {
             let response_body = await getPromise(url);
-            // console.log(response_body);
+            //console.log(response_body);
             return true;
         }
         catch(error) {
@@ -59,9 +78,10 @@ args.forEach(function (file) {
     });
     if(vocabulary.heteronyms.length > 0)
         output.push(vocabulary);
-  });
-});
+  };
+}));
 
 const json = JSON.stringify(output, null, 2);
 fs.writeFileSync('../assets/dict/output.json', json, 'utf8');
 process.exit();
+})();
